8f71ddd0fcd5100b8ea75689a5e25bbc55228faa
ani <aninur.h@gmail.com>
2022-12-07 11:48:45 +0700

    .

---
+++ assets/ceklogin.js
@@ -0,0 +1,29 @@
+document.getElementById("judul").innerText = "Form Login";
+document.getElementById("judul").style.color = "blue";
+// document.getElementsByTagName("h1")[0].innerHTML = "Form Login";
+// document.getElementsByClassName("title")[0].innerHTML = "Form Login";
+// document.getElementById("username").value = "Ani"; mendefinisikan value
+// document.getElementById("password").value = "123";
+let userlog = "admin";
+let passlog = "1234";
+
+//function tanpa paramater
+function ceklogin() {
+  let usernamex, passwordx;
+  usernamex = document.getElementById("user").value; //menampung value
+  passwordx = document.getElementById("password").value;
+  alert("proses cek login: " + usernamex + " - " + passwordx);
+  pesan("Login Berhasil, ", usernamex);
+}
+
+//function dg parameter
+function pesan(message, user) {
+  //login gagal
+  alert(message + user);
+  clear();
+}
+
+function clear() {
+  document.getElementById("user").value = "";
+  document.getElementById("password").value = "";
+}
--- assets/proses.js
+++ assets/proses.js
@@ -1,3 +1,37 @@
 // cara menampilkan output pada JavaScript
 console.log("Hello Javascript");
-document.write("Pemrograman itu mudah");
+document.write("Pemrograman itu mudah <hr>");
+
+let barang = "Bakso";
+let harga = 10000;
+let jmlbeli = 2;
+harga = 25000;
+jmlbeli++;
+let total = harga * jmlbeli;
+
+console.log("Beli " + jmlbeli + " Total " + total);
+console.log();
+console.log("Beli " + barang + " sebanyak : " + jmlbeli + "bungkus");
+
+let mhs = ["daud", "syarif", "fero", "dzaky"];
+// document.write("nama saya " + mhs[2]);
+let nilai1 = 90;
+let nilai2 = 60;
+nilai1++; //91
+nilai1++; //92
+nilai1--;
+// document.write("Hasil Penjumlahan: " + (nilai1 + nilai2));
+// document.write(nilai1 + "<hr>");
+let x = 10;
+let y = 5;
+//x+=y ..... x = x+y
+let h = (x += y); // x= 10+5
+//x+=y ..... x = x+y(15+5)
+// document.write((x += y) + " , " + h); //20, 15
+
+let skor1, skor2, hasil;
+skor1 = 2;
+skor2 = 4;
+//rumus hasil
+hasil = skor2 ** skor1;
+// document.write("-Hasil : " + hasil);
